9674763367e6d87e2731a28b902d8049a34d5273
mostafa <14712022100624@stud.cu.edu.eg>
2025-06-12 17:21:21 +0300

    feat: add error page

---
+++ frontend/Author_App/src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return (
+        <div className="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>We are working to fix this issue. Please try again later.</p>
+        </div>
+    );
+}
+export default ErrorPage;
