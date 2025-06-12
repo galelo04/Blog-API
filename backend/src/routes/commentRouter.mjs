1e691b31fbf8c70454b530ff76eba80972f08f8d
mostafa <14712022100624@stud.cu.edu.eg>
2025-06-12 17:11:56 +0300

    feat: add routes for updated controllers

--- backend/src/routes/commentRouter.mjs
+++ backend/src/routes/commentRouter.mjs
@@ -3,6 +3,6 @@ import commentController from "../controllers/commentController.mjs";
 import { Router } from "express";
 const commentRouter = Router();
 commentRouter.get('/:postId', commentController.getCommentsPost);
-commentRouter.post('/create/:postId', commentController.createComment);
+commentRouter.post('/:postId', commentController.createComment);
 commentRouter.delete('/:id', commentController.deleteComment);
 export default commentRouter;
--- backend/src/routes/postRouter.mjs
+++ backend/src/routes/postRouter.mjs
@@ -2,12 +2,14 @@ import postController from '../controllers/postController.mjs'
 import { Router } from "express";
 const postRouter = Router();
 
+postRouter.get('/author/posts', postController.getAuthorPosts)
+postRouter.get('/author/posts/:id', postController.getPostById)
 postRouter.get('/published', postController.getPublishedPosts)
 postRouter.post('/create', postController.createPost)
 postRouter.patch('/publish/:id', postController.publishPost)
 postRouter.patch('/unpublish/:id', postController.unPublishPost)
 postRouter.patch('/update/content/:id', postController.updatePostContent)
 postRouter.patch('/update/title/:id', postController.updatePostTitle)
-postRouter.delete('/delete', postController.deletePost)
+postRouter.delete('/:id', postController.deletePost)
 
 export default postRouter
